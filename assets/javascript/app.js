4784a4282cf38f5fb5cc5766b98bcd0de01fcf8e
Sam Tricamo <samualtricamo@gmail.com>
2019-07-05 19:39:54 -0500

    timer added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,9 @@
 $(document).ready(function(){
+  
+    
+    $("#remaining-time").hide();
+    $("#start").on('click', trivia.startGame);
+    $(document).on('click', '.option', trivia.guessChecker);
     
   })
   
@@ -40,4 +45,67 @@ $(document).ready(function(){
       q7: 'Shawn Michaels'
     },
     
+    startGame: function(){
+      trivia.currentSet = 0;
+      trivia.correct = 0;
+      trivia.incorrect = 0;
+      trivia.unanswered = 0;
+      clearInterval(trivia.timerId);
+      
+      $('#game').show();
+      
+      $('#results').html('');
+      
+      $('#timer').text(trivia.timer);
+      
+      $('#start').hide();
+  
+      $('#remaining-time').show();
+      
+      trivia.nextQuestion();
+      
+    },
+   
+    nextQuestion : function(){
+      
+   
+      trivia.timer = 10;
+       $('#timer').removeClass('last-seconds');
+      $('#timer').text(trivia.timer);
+      
+      
+      if(!trivia.timerOn){
+        trivia.timerId = setInterval(trivia.timerRunning, 1000);
+      }
+      
+      
+      var questionContent = Object.values(trivia.questions)[trivia.currentSet];
+      $('#question').text(questionContent);
+      
     
+      var questionOptions = Object.values(trivia.options)[trivia.currentSet];
+      
+ 
+      $.each(questionOptions, function(index, key){
+        $('#options').append($('<button class="option btn btn-info btn-lg">'+key+'</button>'));
+      })
+      
+    },
+    timerRunning : function(){
+      if(trivia.timer > -1 && trivia.currentSet < Object.keys(trivia.questions).length){
+        $('#timer').text(trivia.timer);
+        trivia.timer--;
+          if(trivia.timer === 4){
+            $('#timer').addClass('last-seconds');
+          }
+      }
+    
+      else if(trivia.timer === -1){
+        trivia.unanswered++;
+        trivia.result = false;
+        clearInterval(trivia.timerId);
+        resultId = setTimeout(trivia.guessResult, 1000);
+        $('#results').html('<h3>Out of time! The answer was '+ Object.values(trivia.answers)[trivia.currentSet] +'</h3>');
+      }
+   
+    }
